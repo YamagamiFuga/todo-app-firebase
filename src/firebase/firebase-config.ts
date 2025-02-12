6eb5ebba44c78c735522b8f0f2b6a160aeb1a535
yamaff <fuga.19671028@icloud.com>
2025-02-12 22:36:47 +0900

    .env追加

--- src/firebase/firebase-config.ts
+++ src/firebase/firebase-config.ts
@@ -1,16 +1,19 @@
 import { initializeApp } from "firebase/app";
-import "firebase/auth";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDy-2L4Er_rTKCDKCz5suKqornLKTqYxt4",
-    authDomain: "todo-app-6fabb.firebaseapp.com",
-    projectId: "todo-app-6fabb",
-    storageBucket: "todo-app-6fabb.firebasestorage.app",
-    messagingSenderId: "789579566738",
-    appId: "1:789579566738:web:642807fd61784a0592204b",
-    measurementId: "G-LLHG64GKTW"
+apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
+// Firebase 初期化
 const app = initializeApp(firebaseConfig);
-
+export const auth = getAuth(app);
+export const db = getFirestore(app);
 export default app;
